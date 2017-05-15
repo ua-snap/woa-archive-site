8d8845c70de8d7c0d9a1709bcafa3e552abeea60
Carolyn Rosner <carolynrosner1@SNAP-MacBook-Pro-2017.local>
2017-05-15 13:42:23 -0700

    uploads revised archive files for woa

---
+++ README.md
@@ -0,0 +1,7 @@
+# WOA2017 web site archive
+
+This is a simple static HTML site used for the perpetual archive of content and information that was published through the Week of the Arctic 2017 web site.
+
+### Development notes
+
+To work with this site, you just need to clone this repository and open the `index.html` page in your web browser.  No server backend is needed here.
---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+	$("#nanoGallery1").nanoGallery({
+		kind: 'flickr',
+		userID: '133161750@N07',
+		photoset: '72157664247407754',
+
+		thumbnailWidth: 'auto',
+		thumbnailHeight: 200,
+
+		thumbnailHoverEffect: [{ name: 'labelAppear75', duration: 300 }],
+		theme: 'light',
+		thumbnailGutterWidth : 3,
+		thumbnailGutterHeight : 3,
+		i18n: { thumbnailImageDescription: 'View Photo', thumbnailAlbumDescription: 'Open Album' },
+		thumbnailLabel: { display: true, position: 'overImageOnMiddle', align: 'center' }
+	});
+
+	$("#toggleNews").on('click', function() {
+		$(".extra.news").toggle();
+		$("#toggleNews").hide();
+	});
+
+	$("#toggleFellows").on('click', function() {
+		$(".extra.fellows").toggle();
+		$("#toggleFellows").hide();
+	});
+
+	$("#toggleArt").on('click', function() {
+		$(".extra.art").toggle();
+		$("#toggleArt").hide();
+	});
+});
+
